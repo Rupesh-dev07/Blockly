0a992d8ea66b041431af5d240848210c34282eb0
Rupesh-dev07 <68955332+Rupesh-dev07@users.noreply.github.com>
2022-11-30 08:44:48 -0800

    Update main.js

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -14,36 +14,32 @@ var config = {
         }
     },
 
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene:[BootScene, GameScene]
     //Need to use scene : [BootScene, GameScene ] instead of preload and create 
 
 };
 
 var game = new Phaser.Game(config);
 
-function preload() {
-    this.load.image('background', 'assets/level/background-extruded.png');
-    this.load.audio('goldSound', ['assets/audio/Pickup.wav']);
-    this.load.audio('enemyDeath', ['assets/audio/EnemyDeath.wav']);
-    this.load.audio('playerAttack', ['assets/audio/PlayerAttack.wav']);
-    this.load.audio('playerDamage', ['assets/audio/PlayerDamage.wav']);
-    this.load.audio('playerDeath', ['assets/audio/PlayerDeath.wav']);
-    this.load.tilemapTiledJSON('map', 'assets/level/large_level.json')
-}
+// function preload() {
+//     this.load.image('background', 'assets/level/background-extruded.png');
+//     this.load.audio('goldSound', ['assets/audio/Pickup.wav']);
+//     this.load.audio('enemyDeath', ['assets/audio/EnemyDeath.wav']);
+//     this.load.audio('playerAttack', ['assets/audio/PlayerAttack.wav']);
+//     this.load.audio('playerDamage', ['assets/audio/PlayerDamage.wav']);
+//     this.load.audio('playerDeath', ['assets/audio/PlayerDeath.wav']);
+//     this.load.tilemapTiledJSON('map', 'assets/level/large_level.json')
+// }
 
-function create() {
-    this.map = this.make.tilemap({ key: 'map' });
-    this.tiles = this.map.addTilesetImage('background', 'background', 32, 32, 1, 2)
-    this.backgroundLayer = this.map.createStaticLayer('background', this.tiles, 0, 0)
-    this.backgroundLayer.setScale(0.5);
-}
+// function create() {
+//     this.map = this.make.tilemap({ key: 'map' });
+//     this.tiles = this.map.addTilesetImage('background', 'background', 32, 32, 1, 2)
+//     this.backgroundLayer = this.map.createStaticLayer('background', this.tiles, 0, 0)
+//     this.backgroundLayer.setScale(0.5);
+// }
 
-function update() {
+// function update() {
 
-}
+// }
 
 // })

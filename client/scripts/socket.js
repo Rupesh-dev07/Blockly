f47674e7fa512abf21189548181a64d5a192789d
Rupesh-dev07 <68955332+Rupesh-dev07@users.noreply.github.com>
2022-12-02 18:28:43 +0530

    Delete client/scripts directory

--- client/scripts/socket.js
+++
@@ -1,9 +0,0 @@
-var socket = io("http://localhost:2000/", { path: '/socket.io', transports: ['websocket'], secure: true });
-socket.on("connect", function() {
-    console.log(socket.connected); // x8WIv7-mJelg7on_ALbx
-});
-
-socket.on("disconnect", () => {
-    console.log(socket.connected); // undefined
-});
-socket.emit("createRoom", JSON.stringify({ username: username, SessionId: sessionid }));
